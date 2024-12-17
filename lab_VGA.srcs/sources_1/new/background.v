34ef85c0d2913293f1784edeecac679841a300a0
Tsukumi <thurstonstrickland@hotmail.com>
2024-12-18 00:32:40 +0800

    添加rom，添加background模块

---
+++ lab_VGA.srcs/sources_1/new/background.v
@@ -0,0 +1,53 @@
+module background #(
+    parameter ADDR_WIDTH = 15,
+    parameter H_LENGTH   = 200,
+    parameter V_LENGTH   = 150
+) (
+    input clk,
+    input frame_clk,
+    input rstn,
+    input enable,
+    input enable_scroll,
+    input [ADDR_WIDTH-1:0] addr,
+    input [7:0] n,         // 每n个frame_clk更新一次offset，图片向下滚动速度为每秒72/n个像素
+
+    output reg [11:0] rgb
+);
+
+  wire [ADDR_WIDTH-1:0] scroll_addr;
+  reg [ADDR_WIDTH-1:0] offset;
+  wire [7:0] count;  // 计数器
+
+  // 在每个frame_clk上升沿更新计数器和偏移量
+  always @(posedge frame_clk) begin
+    if (!rstn) begin
+      offset <= 0;
+    end else begin
+      if (count == 0&&enable_scroll) begin  // 当计数达到0时，更新offset
+        offset <= (offset + H_LENGTH) % (H_LENGTH * V_LENGTH);
+      end
+    end
+  end
+
+  assign scroll_addr = addr + offset;
+
+  Rom_Background background (
+      .clka (clk),
+      .ena  (enable),
+      .addra(scroll_addr),
+      .douta(rgb)
+  );
+
+  Counter #(8, 255) counter (// 每个frame_clk计数器减1
+      .clk       (frame_clk),
+      .rstn      (rstn),
+      .load_value(n - 1),
+      .enable    (enable_scroll),
+      .count     (count)
+  );
+
+  initial begin
+    offset <= 0;
+  end
+
+endmodule
