232485bae167c95fd0db99f1825ab414d5921bc4
“MoCHIxb” <2901956596@qq.com>
2024-12-19 15:42:39 +0800

    修改player.v,添加了mob.v

--- lab_VGA.srcs/sources_1/new/player.v
+++ lab_VGA.srcs/sources_1/new/player.v
@@ -1,6 +1,7 @@
 // 自机模块
 module Player #(
     parameter ADDR_WIDTH = 15,
+    parameter SPEED_X = 3,
     parameter BUNCE_V = -5,  //回弹初速度
     parameter G_CONST = 1,  //重力加速度
     parameter X_WHITH = 30,  //物体宽
@@ -11,36 +12,38 @@ module Player #(
     input clk,
     input frame_clk,
     input rstn,
-    input [ADDR_WIDTH-1:0] addr,  //输入中心坐标
+    input [H_LENGTH-1:0] addr_x,  //输入起始中心x坐标
+    input [V_LENGTH-1:0] addr_y,  //输入起始中心y坐标
     input [127:0] key_state,
     input enable_scroll,    //借用一下，实现暂停功能
     input collision,       //碰撞信号
     input [7:0] n,         // 每n个frame_clk更新一次offset，图片向下滚动速度为每秒72/n个像素,即刷新率
 
-    output reg [ADDR_WIDTH-1:0] loc //位置
+    output reg [H_LENGTH-1:0] loc_x, //x位置
+    output reg [V_LENGTH-1:0] loc_y  //y位置
 );
 reg  arrow; //判断左右移动方向，取1为左,取0为右
 reg  [7:0] speed_x;  
 reg  [7:0] speed_y;
-//reg  [ADDR_WIDTH-1:0] offset; 
 wire [7:0] count_x;  // 计数器
 wire [7:0] count_y;  // 计数器
 
-// 在每个frame_clk上升沿更新计数器和偏移量,颜色
+// 在每个frame_clk上升沿更新计数器和偏移量
 always @(posedge frame_clk) begin
     if (!rstn) begin
-        loc <= 60;
+        loc_x <= addr_x;
+        loc_y <= addr_y;
     end 
     else begin
       if (count_y == 0) begin  // 计数器为零，y轴移动
-        loc <= (loc + H_LENGTH) % (H_LENGTH * V_LENGTH);
+        loc_y <= (loc_y + 1) % V_LENGTH;
       end
       if (count_x == 0) begin  // 计数器为零，x轴移动
         if (arrow) begin
-            loc <= (loc + 1) % (H_LENGTH);
+            loc_x <= (loc_x + 1) % H_LENGTH;
         end
         else begin
-            loc <= (loc - 1) % (H_LENGTH); 
+            loc_x <= (loc_x - 1) % H_LENGTH;
         end
       end
     end
@@ -50,11 +53,11 @@ end
 always @(posedge frame_clk) begin
     speed_x <= 0;
     if (key_state[1]) begin//键盘输入（具体输入信号未完成）
-        speed_x <= 10;
+        speed_x <= SPEED_X;
         arrow <= 1;
     end
     else if (key_state[0]) begin
-        speed_x <= 10;
+        speed_x <= SPEED_X;
         arrow <= 0;
     end
 end
@@ -84,7 +87,8 @@ Counter #(8, 255) counter_y (// 每个frame_clk计数器减1
 );
 
 initial begin //初始化
-    loc <= 60;
+    loc_x <= addr_x;
+    loc_y <= addr_y;
     arrow <= 0;
     speed_x <= 0;
     speed_y <= 0;
